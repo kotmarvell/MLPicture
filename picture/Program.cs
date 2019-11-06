6cc00a6a26f48635d2bcb3330ddf6fe706aa5705
katerina <e.kozina@itway.company>
2019-11-06 16:17:23 +0300

    new : image processing added

---
+++ picture/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+namespace picture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputFileName = "C:/Users/marve/OneDrive/Рабочий стол/image.jpg";
+            string OutputFileName = "C:/Users/marve/OneDrive/Рабочий стол/image2.jpg";
+            if (!File.Exists(InputFileName))
+                return;
+
+            ColorFloatImage image = ImageIO.FileToColorFloatImage(InputFileName);
+
+            ImageIO.ImageToFile(image, OutputFileName);
+        }
+    }
+}
