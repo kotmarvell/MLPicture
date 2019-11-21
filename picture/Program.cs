e47b0f78ef99a79fa4c5c26a46f682f9f1d531f1
katerina <e.kozina@itway.company>
2019-11-21 16:03:21 +0300

    new : add podd_jpg

--- picture/Program.cs
+++ picture/Program.cs
@@ -11,14 +11,23 @@ namespace picture
     {
         static void Main(string[] args)
         {
-            string InputFileName = "E:/MLPicture/picture/assets/inputs/image.jpg";
-            string OutputFileName = "E:/MLPicture/picture/assets/inputs/image2.jpg";
+            string InputFileName = "E:/MLPicture/picture/assets/inputs/znak.jpg";
+            string OutputFileName = "E:/MLPicture/picture/assets/inputs/finish.jpg";
             if (!File.Exists(InputFileName))
                 return;
 
-            
             ColorFloatImageFormat image = ImageIO.FileToColorFloatImage(InputFileName);
             ImageIO.ImageToFile(image, OutputFileName);
+
+            long count = 0;
+            for(int i = 0; i < image.Height*image.Width; i++)
+            {
+                if((image.rawdata[i].r <= 255) && (image.rawdata[i].r >= 200) && (image.rawdata[i].g >= 200) && (image.rawdata[i].g <= 255) && (image.rawdata[i].b <= 100))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count/300);
         }
     }
 }
