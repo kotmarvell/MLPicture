5a50d859fbf19ef8e888f8d732af76f007421c3c
katerina <e.kozina@itway.company>
2019-11-27 12:56:26 +0300

    new : add helper for image and patterns

---
+++ Picture.BL/PatternHelper.cs
@@ -0,0 +1,29 @@
+﻿using Picture.BLL.Formats;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picture.BLL
+{
+    public class PatternHelper
+    {
+        public PatternHelper(string inputFile)
+        {
+
+        }
+
+        public ColorFloatPixel[,] ColorImage(ColorFloatImageFormat image)
+        {
+            ColorFloatPixel[,] imageData = new ColorFloatPixel[image.Height, image.Width];
+
+            for (int height = 0, countPixel = 0; height < image.Height; ++height)
+            {
+                for (int width = 0; width < image.Width; ++width)
+                {
+                    imageData[height, width] = image.rawdata[countPixel++];
+                }
+            }
+            return imageData;
+        }
+    }
+}
--- picture/Program.cs
+++ picture/Program.cs
@@ -11,23 +11,93 @@ namespace picture
     {
         static void Main(string[] args)
         {
-            string InputFileName = "E:/MLPicture/picture/assets/inputs/znak.jpg";
-            string OutputFileName = "E:/MLPicture/picture/assets/inputs/finish.jpg";
+            string InputFileName = "E:/MLPicture/picture/assets/inputs/Проект ОДД_pages-to-jpg-0027.jpg";
+            string OutputFileName = "E:/MLPicture/picture/assets/inputs/finish1.jpg";
             if (!File.Exists(InputFileName))
                 return;
 
             ColorFloatImageFormat image = ImageIO.FileToColorFloatImage(InputFileName);
-            ImageIO.ImageToFile(image, OutputFileName);
 
-            long count = 0;
-            for(int i = 0; i < image.Height*image.Width; i++)
+            int count = 0;
+            for (int i = 0; i < image.Height * image.Width; i++)
             {
-                if((image.rawdata[i].r <= 255) && (image.rawdata[i].r >= 200) && (image.rawdata[i].g >= 200) && (image.rawdata[i].g <= 255) && (image.rawdata[i].b <= 100))
+                if ((image.rawdata[i].r <= 255) && (image.rawdata[i].r >= 200) && (image.rawdata[i].g >= 200) && (image.rawdata[i].g <= 255) && (image.rawdata[i].b <= 100))
                 {
-                    count++;
+                    image.rawdata[i].r = 255;
+                    image.rawdata[i].g = 255;
+                    image.rawdata[i].b = 0;
                 }
             }
-            Console.WriteLine(count/300);
+            ImageIO.ImageToFile(image, OutputFileName);
+            ColorFloatPixel[,] imageData = new ColorFloatPixel[image.Height, image.Width];
+
+            for (int i = 0, k = 0; i < image.Height; ++i)
+            {
+                for (int l = 0; l < image.Width; ++l)
+                {
+                    imageData[i, l] = image.rawdata[k++];
+                }
+            }
+
+            int[,] data = new int[image.Height, image.Width];
+
+            string InputPattern = "E:/MLPicture/picture/assets/inputs/finish.jpg";
+            ColorFloatImageFormat pattern = ImageIO.FileToColorFloatImage(InputPattern);
+            ColorFloatPixel[,] patternData = new ColorFloatPixel[pattern.Height, pattern.Width];
+            
+            //for (int i = 0, k = 0; i < pattern.Height; ++i)
+            //{
+            //    for (int l = 0; l < pattern.Width; ++l)
+            //    {
+            //        patternData[i, l] = pattern.rawdata[k++];
+            //    }
+            //}
+            //int m = 0;
+            //int z = 0;
+            //int max = 0;
+            //int countM = 0;
+            //for (int height = 0; height < image.Height - 47; height++)
+            //{
+            //    for (int width = 0; width < image.Width - 48; width++)
+            //    {
+            //        count = 0;
+            //        m = 0;
+            //        z = 0;
+            //        for (int i = height; i < height + 47; i++)
+            //        {
+            //            z = 0;
+            //            for (int l = width; l < width + 48; l++)
+            //            {
+            //                if ((patternData[m, z].r == imageData[i, l].r) && (patternData[m, z].g == imageData[i, l].g) && (patternData[m, z].b == imageData[i, l].b))
+            //                {
+            //                    count++;
+            //                }
+            //                z++;
+            //            }
+            //            m++;
+            //        }
+            //        if(count > max)
+            //        {
+            //            max = count;
+            //        }
+            //        data[height, width] = count;
+            //    }
+            //}
+
+
+            //for (int height = 0; height < image.Height; height++)
+            //{
+            //    for (int width = 0; width < image.Width; width++)
+            //    {
+            //        if(data[height,width] >= max-1)
+            //        {
+            //            Console.WriteLine(height + " " + width);
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine(countM);
+
         }
     }
 }
