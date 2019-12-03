6297736ec81474825e04ea74b8b6c97652a8e9c2
katerina <e.kozina@itway.company>
2019-12-03 12:40:42 +0300

    new : add realized to image class and result image class

--- Picture.DAL/Models/Image.cs
+++ Picture.DAL/Models/Image.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Picture.DAL.Formats;
+using Picture.DAL.IO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,13 @@ namespace Picture.DAL.Models
 {
     public class Image
     {
+        public string NameFile { get; set; }
+        public ColorFloatImageFormat image { get; set; }
+        public ColorFloatPixel[,] matrixPixelImage { get; set; }
+        public Image(string ImageFileName = "")
+        {
+            NameFile = ImageFileName;
+            image = ImageIO.FileToColorFloatImage(ImageFileName);
+        }
     }
 }
--- Picture.DAL/Models/ImageResult.cs
+++ Picture.DAL/Models/ImageResult.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Picture.DAL.Formats;
+using Picture.DAL.IO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,13 @@ namespace Picture.DAL.Models
 {
     public class ImageResult
     {
+        public string NameFile { get; set; }
+        public ColorFloatImageFormat image { get; set; }
+        public ColorFloatPixel[,] matrixPixelImage { get; set; }
+        public ImageResult(string ResultFileName = "")
+        {
+            NameFile = ResultFileName;
+            image = ImageIO.FileToColorFloatImage(ResultFileName);
+        }
     }
 }
